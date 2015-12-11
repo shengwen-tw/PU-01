ce6259fd7a968fe0f9badf2d325eeffa2e970ab5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2015-12-11 14:07:49 +0000

    Divide vertical tail's area by two

--- twin_boom_design.py
+++ twin_boom_design.py
@@ -20,6 +20,7 @@ TVCv = 0.05
 Lv = 0.5
 v_tail_AR = 2
 v_tail_area = TVCv * wing_area * wing_span / Lv
+v_tail_area = v_tail_area / 2
 #v_tail_span = (v_tail_area * v_tail_AR) ** 0.5
 #v_tail_chord = v_tail_area / h_tail_span
 v_tail_chord = h_tail_chord
@@ -27,20 +28,24 @@ v_tail_span = v_tail_area / v_tail_chord
 
 ###################################################################
 
+print "__________________________________________________"
+
 print "wing span = " + str(wing_span)
 print "wing chord = " + str(wing_chord)
 print "wing area = " + str(wing_area)
 print "wing Aspect ratio= " + str(wing_AR)
 
-print "_______________________________________"
+print "__________________________________________________"
 
-print "Horizontal tail area: " + str(h_tail_area)
+print "Horizontal tail area: " + str(h_tail_area) + "m^2"
 print "Horizontal tail span: " + str(h_tail_span * 100) + "cm"
 print "Horizontal tail chord: " + str(h_tail_chord * 100) + "cm"
 
-print "_______________________________________"
+print "__________________________________________________"
 
-print "Vertical tail area: " + str(v_tail_area)
+print "Vertical tail area: " + str(v_tail_area) + "m^2 <--- Twin fin"
 print "Vertical tail span: " + str(v_tail_span * 100) + "cm"
 print "Vertical tail chord: " + str(v_tail_chord * 100) + "cm"
+
+print "__________________________________________________"
 
